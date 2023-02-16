d564b000eee4cddb19aa5c4bf025d257bfb7139d
fsogonov <113305871+fsogonov@users.noreply.github.com>
2023-02-15 20:59:38 -0500

    Add files via upload

--- MovieDriver.java
+++ MovieDriver.java
@@ -10,16 +10,20 @@ public class MovieDriver {
 		Scanner inpt = new Scanner(System.in);
 		Movie m = new Movie();
 		
+		String answer = "y";
+		while (answer.equals("y")) {
+			System.out.println("Enter the name of the movie: ");
+			m.setTitle(inpt.nextLine());
+			System.out.println("Enter the rating of the movie: ");
+			m.setRating(inpt.nextLine());
+			System.out.println("Enter the number of tickets sold for the movie: ");
+			m.setSoldTickets(inpt.nextInt());
 		
-		System.out.println("Enter the name of the movie: ");
-		m.setTitle(inpt.nextLine());
-		System.out.println("Enter the rating of the movie: ");
-		m.setRating(inpt.nextLine());
-		System.out.println("Enter the number of tickets sold for the movie: ");
-		m.setSoldTickets(inpt.nextInt());
-		
-		System.out.println("Title: '" + m.getTitle() + "', rating: " + m.getRating() + ", tickets sold: " + m.getSoldTickets());
-		
+			System.out.println("Title: '" + m.getTitle() + "', rating: " + m.getRating() + ", tickets sold: " + m.getSoldTickets());
+			System.out.println("Do you want to continue? (y or n)");
+			inpt.nextLine();
+			answer = inpt.nextLine();
+		}	
 		inpt.close();
 	}
 }
